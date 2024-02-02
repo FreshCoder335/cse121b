7d0bf2443e3b6dca4e856fbb6b022a58b1fe0be1
FreshCoder335 <97472761+FreshCoder335@users.noreply.github.com>
2024-02-01 17:06:51 -0800

    Finished Program task

---
+++ scripts/w04-task.js
@@ -0,0 +1,85 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let MyProfile = 
+{
+    name: "Jeremiah Powell",
+    profilePicture : ('images/personalpicture.png'),
+    favoriteFoods: 
+    [
+        'barbecue', 'burgers', 'sandwiches', 'soup'
+    ],
+    hobbies: ['Gaming', 'Snowboarding', 'Firearms'],
+    placesLived: []
+};
+
+
+
+
+/* Populate Profile Object with placesLive objects */
+MyProfile.placesLived.push(
+    {
+        place: 'Logan, Utah',
+        length: 'six months',
+    },
+    {
+        place: 'Idaho Falls, Idaho',
+        length: 'Six Years',
+    },
+    {
+        place: 'Rigby, Idaho',
+        length: 'Sixteen Years',
+    },
+    {
+        place: 'Independece, Missouri',
+        length: 'Two years',
+    },
+    {
+        place: 'Rexburg, Idaho',
+        length: 'Two years'
+    }
+);
+
+
+
+/* DOM Manipulation - Output */
+/* Name */
+document.querySelector('#name').textContent = MyProfile.name;
+
+
+/* Photo with attributes */
+document.getElementById('photo').src = MyProfile.profilePicture;
+document.getElementById('photo').alt = MyProfile.name;
+
+/* Favorite Foods List*/
+MyProfile.favoriteFoods.forEach(food => {
+    let li = document.createElement('li');
+    li.textContent = food;
+    document.querySelector('#favorite-foods').appendChild(li);
+});
+
+
+/* Hobbies List */
+MyProfile.hobbies.forEach(hobby => {
+    let ul = document.createElement('ul');
+    ul.textContent = hobby;
+    document.querySelector('#hobbies').appendChild(ul);
+});
+
+
+/* Places Lived DataList */
+MyProfile.placesLived.forEach(place => {
+    
+    let dt = document.createElement('dt');
+    dt.textContent = place.place;
+
+   
+    let dd = document.createElement('dd');
+    dd.textContent = place.length;
+
+    
+    document.querySelector('#places-lived').appendChild(dt);
+    document.querySelector('#places-lived').appendChild(dd);
+});
+
+
